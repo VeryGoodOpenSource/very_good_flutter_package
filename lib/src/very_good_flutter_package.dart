dac3f67e2064ec5b50d01cb731178598a2442c59
Felix Angelov <felangelov@gmail.com>
2022-07-22 12:19:09 -0400

    feat: create a very good flutter package

---
+++ lib/src/very_good_flutter_package.dart
@@ -0,0 +1,7 @@
+/// {@template very_good_flutter_package}
+/// A Very Good Flutter package created by Very Good Ventures.
+/// {@endtemplate}
+class VeryGoodFlutterPackage {
+  /// {@macro very_good_flutter_package}
+  const VeryGoodFlutterPackage();
+}
---
+++ test/src/very_good_flutter_package_test.dart
@@ -0,0 +1,12 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter_test/flutter_test.dart';
+import 'package:very_good_flutter_package/very_good_flutter_package.dart';
+
+void main() {
+  group('VeryGoodFlutterPackage', () {
+    test('can be instantiated', () {
+      expect(VeryGoodFlutterPackage(), isNotNull);
+    });
+  });
+}
