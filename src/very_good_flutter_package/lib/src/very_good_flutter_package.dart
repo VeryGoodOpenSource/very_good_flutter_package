50e767f9a36b8a3be7379f4fbd92904d095bf76d
Alejandro Santiago <dev@alestiago.com>
2023-11-28 14:10:01 +0000

    refactor: remove generator script (#72)

--- src/very_good_flutter_package/lib/src/very_good_flutter_package.dart
+++
@@ -1,7 +0,0 @@
-/// {@template very_good_flutter_package}
-/// A Very Good Flutter package created by Very Good Ventures.
-/// {@endtemplate}
-class VeryGoodFlutterPackage {
-  /// {@macro very_good_flutter_package}
-  const VeryGoodFlutterPackage();
-}
